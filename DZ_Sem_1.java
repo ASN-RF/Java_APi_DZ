60d5c72a33cca0e034c7f5c8f6228855798b0029
Gr_n RF <gr.nrfinfo@gmail.com>
2023-06-18 22:40:29 +0600

    Added Task 4 from DZ_1

--- DZ_Sem_1.java
+++ DZ_Sem_1.java
@@ -1,5 +1,5 @@
 
-// Домашенее задание 1.
+// Домашнее задание 1.
 import java.util.Random;
 import java.util.Arrays;
 
@@ -43,13 +43,11 @@ public class DZ_Sem_1 {
         System.out.println("Диапазон: от " + i + " до " + Short.MAX_VALUE);
         // Определяем длину массива
         int dlina_m1 = 0;
-        for (int j = 0; j < Short.MAX_VALUE; j++) {
+        for (int j = i; j < Short.MAX_VALUE; j++) {
             if (j % n == 0) {
                 dlina_m1++;
             }
         }
-        System.out.println("Длина массива: " + dlina_m1);
-
         // Создаем и заполнямем массив м1
         int [] m1 = new int [dlina_m1];
         int index_m1 = 0;
@@ -59,23 +57,45 @@ public class DZ_Sem_1 {
                 index_m1++; 
             }
         }
-        // Выводим в консоль массив м1
+        // Выводим в консоль массив м1 (1 вариант)
         /*for (int l = 0; l <= dlina_m1; l++) {
             System.out.print(m1[l] + ", ");
         }*/
-        System.out.println("Длина массива: " + m1.length); // Почуме при выводе массива в консоль появляются в конце нули?
-        System.out.println(Arrays.toString(m1));
+        System.out.println("Длина массива: " + m1.length); 
+        //System.out.println(Arrays.toString(m1));
+        System.out.println();
 
-        
+        // Задание 4. Найти все некратные n числа в диапазоне от Short.MIN_VALUE до i и сохранить в массив m2.
+        System.out.println("Задание 4. Решение:");
+        System.out.println("Дано: n = " + n);
+        System.out.println("Диапазон: от " + Short.MIN_VALUE + " до " + i);
+        // Определяем длину массива
+        // 1 вариант  - длина массива
+        int dlina_m2 = 0;
+        for (int j = i; j > Short.MIN_VALUE; j--) {
+            if (j % n != 0) {
+                dlina_m2++;
+            }
+        }
+        // 2 вариант - длина массива
+       /*  int dlina_m3 = 0;
+        for (int l = Short.MIN_VALUE; l < i; l++) {
+            if (l % n != 0) {
+                dlina_m3++;
+            }
+        } */
 
-
-
-
-
-
-
-        
-          
+        // Создаем и заполнямем массив м2
+        int [] m2 = new int [dlina_m2];
+        int index_m2 = 0;
+        for (int j = i; j > Short.MIN_VALUE; j--) {
+            if (j % n != 0) {
+                m2[index_m2] = j;
+                index_m2++; 
+            }
+        }
+        // Выводим в консоль массив м2
+        System.out.println("Длина массива: " + m2.length); 
+       // System.out.println(Arrays.toString(m2));  
     }
-
 }
